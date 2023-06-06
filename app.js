2b356a0b4009f2d8ee53f72521abd4a2e93fe584
Nasko-5 <sect0r.mainacc@gmail.com>
2023-06-06 19:42:13 +0300

    Initial Commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const createError = require('http-errors');
+const morgan = require('morgan');
+const pug = require("pug")
+
+
+require('dotenv').config();
+
+const app = express();
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan('dev'));
+
+app.get('/', async (req, res, next) => {
+  res.render('index', { title: 'awsome test project'})
+});
+
+app.use('/api', require('./routes/api.route'));
+
+app.use((req, res, next) => {
+  next(createError.NotFound());
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send({
+    status: err.status || 500,
+    message: err.message,
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`🚀 @ http://localhost:${PORT}`));
