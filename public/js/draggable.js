6f126ba379df51f234435419d0d6b2b231e7de8c
Nasko-5 <sect0r.mainacc@gmail.com>
2023-06-08 16:26:43 +0300

    opened windows can close now

--- public/js/draggable.js
+++ public/js/draggable.js
@@ -2,4 +2,10 @@
 
 $(document).ready(function() {
     $(".draggable").draggable();
+
+    // target all buttons with the aria label close, and when it is clicked, go to the window and hide it
+    $("button").attr("aria-label", "Close").click(function() {
+        $(this).parent().parent().parent().remove();
+    });
+    
 });
